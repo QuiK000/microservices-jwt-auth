4b94b58346ff21e336cfe83ca8964585e6ff6a99
QuiK000 <teamquiksteam@gmail.com>
2026-03-28 19:23:03 +0200

    Add RoleRepository with custom findByName method

---
+++ services/auth-service/src/main/java/com/dev/quikkkk/auth_service/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.dev.quikkkk.auth_service.repository;
+
+import com.dev.quikkkk.auth_service.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, String> {
+    Optional<Role> findByName(String roleName);
+}
