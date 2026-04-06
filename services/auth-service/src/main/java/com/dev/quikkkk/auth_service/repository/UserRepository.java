1425d28c162c2d590c75affd6a443523ef46e850
QuiK000 <teamquiksteam@gmail.com>
2026-04-06 20:16:02 +0300

    Add findByEmail method to UserRepository

--- services/auth-service/src/main/java/com/dev/quikkkk/auth_service/repository/UserRepository.java
+++ services/auth-service/src/main/java/com/dev/quikkkk/auth_service/repository/UserRepository.java
@@ -4,7 +4,11 @@ import com.dev.quikkkk.auth_service.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByEmailIgnoreCase(String email);
+
+    Optional<User> findByEmail(String email);
 }
