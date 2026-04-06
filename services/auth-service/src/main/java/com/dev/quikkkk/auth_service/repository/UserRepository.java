c239408f19b14b0e890410f9a104aa849f841759
QuiK000 <teamquiksteam@gmail.com>
2026-04-06 19:50:01 +0300

    Add UserRepository with email existence check

---
+++ services/auth-service/src/main/java/com/dev/quikkkk/auth_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.dev.quikkkk.auth_service.repository;
+
+import com.dev.quikkkk.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmailIgnoreCase(String email);
+}
